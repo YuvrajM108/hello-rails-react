ac99184c28fc901edb430e5f8fbe57d512dd413f
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-11-13 00:28:28 -0400

    Create API endpoint for greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :greetings
+  end
+
+  root 'greetings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
