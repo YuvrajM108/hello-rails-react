e0545fc31b35160d486d4d5d59e1c86e2a8c29a0
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-11-13 15:17:37 -0400

    Set root route to be greetings index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     resources :greetings
   end
 
-  root 'greetings#index'
+  root 'api/greetings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
